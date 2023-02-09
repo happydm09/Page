db7c4bba1d20a82214e6cabcfb8c67a49734b974
DM <100419204+happydm09@users.noreply.github.com>
2023-02-09 23:27:50 +0900

    Update drag.js

--- test/drag.js
+++ test/drag.js
@@ -1,11 +1,15 @@
-  function allowUserSelect(element) {
-        element.setAttribute('style', '-webkit-user-select: auto !important');
-        element.setAttribute('style', 'user-select: auto !important');
-        return element
- }
-function clearHandlers() {
-        html.onselectstart = html.oncopy = html.oncut = html.onpaste = html.onkeydown = html.oncontextmenu = html.onmousemove = body.oncopy = body.oncut = body.onpaste = body.onkeydown = body.oncontextmenu = body.onmousedown = body.onmousemove = body.onselectstart = body.ondragstart = doc.onselectstart = doc.oncopy = doc.oncut = doc.onpaste = doc.onkeydown = doc.oncontextmenu = doc.onmousedown = doc.onmouseup = window.onkeyup = window.onkeydown = null;
-        allowUserSelect(html);
-        allowUserSelect(body)
+var body = doc.body;
+var html = doc.documentElement;
+
+function allowUserSelect(element) {
+    element.setAttribute('style', '-webkit-user-select: auto !important');
+    element.setAttribute('style', 'user-select: auto !important');
+    return element
 }
+function clearHandlers() {
+    html.onselectstart = html.oncopy = html.oncut = html.onpaste = html.onkeydown = html.oncontextmenu = html.onmousemove = body.oncopy = body.oncut = body.onpaste = body.onkeydown = body.oncontextmenu = body.onmousedown = body.onmousemove = body.onselectstart = body.ondragstart = doc.onselectstart = doc.oncopy = doc.oncut = doc.onpaste = doc.onkeydown = doc.oncontextmenu = doc.onmousedown = doc.onmouseup = window.onkeyup = window.onkeydown = null;
+    allowUserSelect(html);
+    allowUserSelect(body)
+}
+
 clearHandlers();
