5b9cd663a3daeb14215148cd3056b401f8d7a383
DM <100419204+happydm09@users.noreply.github.com>
2023-03-12 12:01:33 +0900

    Update open.js

--- test/open.js
+++ test/open.js
@@ -1,1 +1,11 @@
-window.open('view-source:' + window.location.href )
+function copyClipboard(text) {
+  const textarea = document.createElement('textarea');
+  textarea.textContent = text;
+  document.body.append(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  textarea.remove();
+}
+
+copyClipboard('view-source:' + window.location.href)
+alert('Copied Code View URL')
