d4ea825e9ccad82075f9f7c050180cc3b97189d5
DM <100419204+happydm09@users.noreply.github.com>
2023-04-11 00:07:54 +0900

    Create bg.js

---
+++ Data/bg.js
@@ -0,0 +1,11 @@
+function Data(c){
+  var date = c.getAttribute('date')
+  var get = c.getAttribute('get')
+  var h = c.getAttribute('h')
+  var src = c.getAttribute('src')
+  
+  var m = [document.querySelector("#m-title"), document.querySelector("#m-content")]
+  m[0].innerHTML = `<img src='${src}' class='img-fluid'></img>${c.getAttribute('title')}`
+  m[1].innerHTML = `얻은 날짜: ${date} <br> ${get}번째, <a href='${h}'>링크</a>`
+  $('#modal').modal('show');
+}
