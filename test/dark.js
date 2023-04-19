53d23181094d0352500e892200c41d9f074ebeba
DM <100419204+happydm09@users.noreply.github.com>
2023-04-20 08:01:42 +0900

    Create dark.js

---
+++ test/dark.js
@@ -0,0 +1,6 @@
+let cibSerp = document.getElementsByTagName("cib-serp");
+
+    for (let i = 0; i < cibSerp.length; i++) {
+       cibSerp[i].design.colorScheme=1;
+       cibSerp[i].alignment="center";
+   }
