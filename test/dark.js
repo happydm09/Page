bca3584a53ca4ca28b1ff1c2c06adeca39946e01
DM <100419204+happydm09@users.noreply.github.com>
2023-04-20 16:44:26 +0900

    Update dark.js

--- test/dark.js
+++ test/dark.js
@@ -1,6 +1,6 @@
-let cibSerp = document.getElementsByTagName("cib-serp");
+let cibSerp_el = document.getElementsByTagName("cib-serp");
 
-    for (let i = 0; i < cibSerp.length; i++) {
-       cibSerp[i].design.colorScheme=1;
-       cibSerp[i].alignment="center";
+    for (let i = 0; i < cibSerp_el.length; i++) {
+       cibSerp_el[i].design.colorScheme=1;
+       cibSerp_el[i].alignment="center";
    }
