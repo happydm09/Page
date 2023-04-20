7ea7a26076f7d9e58dcc0d3cafea72652e9f068c
DM <100419204+happydm09@users.noreply.github.com>
2023-04-20 16:47:42 +0900

    Update dark.js

--- test/dark.js
+++ test/dark.js
@@ -1,6 +1,26 @@
-let cibSerp_el = document.getElementsByTagName("cib-serp");
-
-    for (let i = 0; i < cibSerp_el.length; i++) {
-       cibSerp_el[i].design.colorScheme=1;
-       cibSerp_el[i].alignment="center";
-   }
+(function() {
+    let timer = null;
+    let cibSerp = document.getElementsByTagName("cib-serp");
+    let cibSerpCount = 0;
+    const maxCibSerpCount = 2; // bing chat on the chat tab, and right side of search result
+    let maxRetryCount = 25;
+    function delay(interval = 200) {
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+        timer = setTimeout(() => {
+            maxRetryCount--;
+            console.log(cibSerp.length);
+            if (cibSerp.length > 0 && cibSerpCount < maxCibSerpCount && cibSerpCount < cibSerp.length) {
+                for (let i = cibSerpCount; i < cibSerp.length; i++) {
+                    cibSerp[i].design.colorScheme=1;
+                    cibSerp[i].alignment="center";
+                }
+                cibSerpCount = cibSerp.length;
+            }
+            if (cibSerpCount < maxCibSerpCount && maxRetryCount > 0) delay();
+        }, interval);
+    }
+    delay();
+})();
