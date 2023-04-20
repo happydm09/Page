6ae99f40fa90d60103f5521493af47a2632f3567
DM <100419204+happydm09@users.noreply.github.com>
2023-04-20 19:43:18 +0900

    Update dark.js

--- test/dark.js
+++ test/dark.js
@@ -1,26 +1,12 @@
-(function() {
-    let timer = null;
-    let cibSerp = document.getElementsByTagName("cib-serp");
-    let cibSerpCount = 0;
-    const maxCibSerpCount = 2; // bing chat on the chat tab, and right side of search result
-    let maxRetryCount = 25;
-    function delay(interval = 200) {
-        if (timer) {
-            clearTimeout(timer);
-            timer = null;
-        }
-        timer = setTimeout(() => {
-            maxRetryCount--;
-            console.log(cibSerp.length);
-            if (cibSerp.length > 0 && cibSerpCount < maxCibSerpCount && cibSerpCount < cibSerp.length) {
-                for (let i = cibSerpCount; i < cibSerp.length; i++) {
-                    cibSerp[i].design.colorScheme=1;
-                    cibSerp[i].alignment="center";
-                }
-                cibSerpCount = cibSerp.length;
-            }
-            if (cibSerpCount < maxCibSerpCount && maxRetryCount > 0) delay();
-        }, interval);
-    }
-    delay();
-})();
+Dark = setInterval(function() {
+  var cibSerp = document.getElementsByTagName("cib-serp");
+  var n = 0
+  for (let i = 0; i < cibSerp.length; i++) {
+    cibSerp[i].design.colorScheme=1;
+    cibSerp[i].alignment="center";
+	n = 1
+  }
+  if (n != 0) {
+	clearInterval(Dark);
+  }
+}, 200);
