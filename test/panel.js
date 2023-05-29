0ef71f6293d81f866ad0580c2bf3bb47ea82f0ee
DM <happydm09@naver.com>
2023-05-30 00:06:28 +0900

    Create panel.js

---
+++ test/panel.js
@@ -0,0 +1,10 @@
+Panels = setInterval(function() {
+  var n = 0
+  try{
+	var firstElement = document.querySelector(".cib-serp-main").shadowRoot;
+	var panel = firstElement.querySelector("cib-side-panel");
+	panel.setAttribute('style', 'display: none')
+	n = 1
+	}catch(e){}
+  if (n != 0) { clearInterval(Panels) }
+}, 200);
