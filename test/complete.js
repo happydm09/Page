fde500853eb9034f1456257efb1bb15e7faa16ad
dongmin <happydm09@naver.com>
2024-05-24 18:23:10 +0900

    Create complete.js

---
+++ test/complete.js
@@ -0,0 +1,31 @@
+let wpRequire;
+window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => { wpRequire = req; }]);
+
+let api = Object.values(wpRequire.c).find(x => x?.exports?.getAPIBaseURL).exports.HTTP;
+let ApplicationStreamingStore = Object.values(wpRequire.c).find(x => x?.exports?.default?.getStreamerActiveStreamMetadata).exports.default;
+let QuestsStore = Object.values(wpRequire.c).find(x => x?.exports?.default?.getQuest).exports.default;
+let encodeStreamKey = Object.values(wpRequire.c).find(x => x?.exports?.encodeStreamKey).exports.encodeStreamKey;
+let sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+let quest = [...QuestsStore.quests.values()].find(x => x.userStatus?.enrolledAt && !x.userStatus?.completedAt)
+if(!quest) {
+	console.log("You don't have any uncompleted quests!")
+} else {
+	let streamId = encodeStreamKey(ApplicationStreamingStore.getCurrentUserActiveStream())
+	let secondsNeeded = quest.config.streamDurationRequirementMinutes * 60
+	let heartbeat = async function() {
+		console.log("Completing quest", quest.config.messages.gameTitle, "-", quest.config.messages.questName)
+		while(true) {
+			let res = await api.post({url: `/quests/${quest.id}/heartbeat`, body: {stream_key: streamId}})
+			let progress = res.body.stream_progress_seconds
+			
+			console.log(`Quest progress: ${progress}/${secondsNeeded}`)
+			
+			if(progress >= secondsNeeded) break;
+			await sleep(30 * 1000)
+		}
+		
+		console.log("Quest completed!")
+	}
+	heartbeat()
+}
